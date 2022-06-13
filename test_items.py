32babad317bcda86695ba70e7b044613b709e0d7
Romkka <romkasharaf@gmail.com>
2022-06-13 15:03:26 +0400

    Fixed test_items.py assert message and find_elements method

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
+from selenium.webdriver.common.by import By
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_customer_should_see_add_to_basket_button_on_the_item_page(browser):
     browser.get(link)
-    button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
-    assert button, "\n'Add to basket' button is not presented at the item page"
+    button = browser.find_elements(By.CSS_SELECTOR,'button.btn-add-to-basket')
+    assert button, "'Add to basket' button is not presented at the item page"
 
